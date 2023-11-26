a94970e0b93479fe6e3d703e6aded0f637eaae8b
user <feda.avdeev@yandex.ru>
2023-11-26 20:12:11 +0300

    add: add get_selected_text function

--- extension.js
+++ extension.js
@@ -1,15 +1,27 @@
 const vscode = require('vscode');
 
 
+function get_selected_text() {
+	const editor = vscode.window.activeTextEditor;
+	const selection = editor.selection;
+
+	if (selection && !selection.isEmpty) {
+    	const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+    	const highlighted = editor.document.getText(selectionRange);
+		return highlighted;
+	}
+	return "";
+}
+
+function extract() {
+	
+	vscode.window.showInformationMessage(get_selected_text());
+}
+
 function activate(context) {
-
 	console.log('Congratulations, your extension "function-extractor" is now active!');
-	let disposable = vscode.commands.registerCommand('function-extractor.extract', function () {
-
-		vscode.window.showInformationMessage('Hello World from function extractor!');
-	});
-
-	context.subscriptions.push(disposable);
+	const extract_func = vscode.commands.registerCommand("function-extractor.extract", extract);
+	context.subscriptions.push(extract_func);
 }
 
 function deactivate() {}
