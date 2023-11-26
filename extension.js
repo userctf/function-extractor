1123f1e60370e19553ff80ba20418b79adfec753
user <feda.avdeev@yandex.ru>
2023-11-26 23:19:10 +0300

    logic: move all file edits in one function

--- extension.js
+++ extension.js
@@ -1,50 +1,47 @@
+'use strict'
 const vscode = require('vscode');
 
 
-function get_selected_text(editor) {
+function get_selected_block(editor) {
 	const selection = editor.selection;
 
 	if (selection && !selection.isEmpty) {
     	const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
-    	const highlighted = editor.document.getText(selectionRange);
-		return highlighted;
+    	return selectionRange
 	}
-	return "";
-}
-
-function paste_at_the_end(editor, content) {
-	const document = editor.document;
-    const lastLine = document.lineAt(document.lineCount - 1);
-    const endPosition = new vscode.Position(document.lineCount - 1, lastLine.range.end.character);
-
-	editor.edit(editBuilder => {
-        editBuilder.replace(new vscode.Range(endPosition, endPosition), "\n"+ content + "\n");
-    });
+	return null;
 }
 
 async function get_user_input(prompt) {
     const userInput = await vscode.window.showInputBox({ prompt: prompt });
-
 	return userInput;
 }
 
 
-function extract() {
+async function extract() {
 	const editor = vscode.window.activeTextEditor;
     if (!editor) {
         return; // No active text editor
     }
 
-	const func_name = get_user_input("Enter new function name");
+	const func_name = await get_user_input("Enter new function name");
+	const old_code_range = get_selected_block(editor); // remove old code block
+	const old_code_block = editor.document.getText(old_code_range)
 
 
-	paste_at_the_end("Test text at the end of the file")
+	const document = editor.document;
+    const lastLine = document.lineAt(document.lineCount - 1);
+    const endPosition = new vscode.Position(document.lineCount - 1, lastLine.range.end.character);
+					 
 
-	vscode.window.showInformationMessage(get_selected_text(editor));
+	editor.edit(editBuilder => {
+		// remove old code block
+		editBuilder.replace(old_code_range, '\n' + func_name + '();\n');
 
-	editor.edit((selectedText) => {
-    	selectedText.replace(editor.selection, "Aboba");
-	})
+		// add function at the end
+		editBuilder.replace(new vscode.Range(endPosition, endPosition), 
+				'\n' + "function " + func_name + "() {\n" +  old_code_block + "\n}" + '\n')
+	});
 }
 
 function activate(context) {
